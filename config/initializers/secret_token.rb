7e286fc163eff481f7086e80109fd8a029e1b256
Luca Mearelli <l.mearelli@spazidigitali.com>
2014-03-02 18:35:23 +0100

    adds gems and configurations

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-BlogTest::Application.config.secret_key_base = 'c1a56978d7891d4020bc6a0a108390bc599d7428f44a69210402c9f2d751c7f017ce79dbfc635348fc83dbc68343f5b4b8131b9372f00b7c5f54cbba4693354e'
+BlogTest::Application.config.secret_key_base = ENV['SECRET_TOKEN']
